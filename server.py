24753bdb360102138ef1caec09db1b443c7d93ee
Sreedhayan <k.sreedhayan@gmail.com>
2025-11-30 15:40:31 +0530

    main python code

--- server.py
+++ server.py
@@ -7,3 +7,4 @@ handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), handler) as httpd:
     print(f"Serving at http://localhost:{PORT}")
     httpd.serve_forever()
+    
